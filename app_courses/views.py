3d11cc707c819a6443f84c33aa82b6f55804d903
phritnakgate <boss5500216@gmail.com>
2023-01-27 10:56:39 +0700

    Create base template

--- app_courses/views.py
+++ app_courses/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 
+all_courses = [
+    {'id':'c_programming', 'title': 'Fundamental C Programming', 'teacher': 'A.Thanunchai'},
+    {'id':'arduino', 'title': 'Fundamental Arduino', 'teacher': 'A.Thana'},
+    {'id':'java_programming', 'title': 'Fundamental Java Programming', 'teacher': 'P.Boss'}
+]
+
 # Create your views here.
 def courses(request):
-    return render(request, 'app_courses/courses.html')
+    context = {'all_courses': all_courses}
+    return render(request, 'app_courses/courses.html', context)
 
 def course(request, course_id):
     return render(request, 'app_courses/course.html',context={'course_id': course_id})
